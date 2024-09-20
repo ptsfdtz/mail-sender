949426343e982d588e693e6db7a22dae5311885a
ptsfdtz <pitousanfadetuzi@gmail.com>
2024-09-20 09:47:44 +0800

    feat:输入录取未录取即可

--- main.py
+++ main.py
@@ -6,25 +6,21 @@ from utils.smtp_utils import get_smtp_settings
 
 class EmailSenderApp:
     def __init__(self):
-        self.file_path = './data/未录取名单.xlsx'
+        self.file_path = f'./data/{input("录取 or 未录取")}名单.xlsx'
         self.username, self.password = read_credentials('./key.txt')
-
+        print(f"名单 {self.file_path}")
         recipient_data = read_recipient_list(self.file_path)
 
         if recipient_data:
             self.send_emails(self.username, self.password, recipient_data)
-        else:
-            print("读取收件人名单失败，程序退出。")
 
     def send_emails(self, email_address, email_password, recipient_data):
         total_recipients = len(recipient_data)
         
-        with tqdm(total=total_recipients, desc="发送进度", unit="封") as pbar:
+        with tqdm(total=total_recipients, desc="sending emails", unit="emails") as pbar:
             for i, (recipient_name, recipient_email) in enumerate(recipient_data.items(), 1):
                 self.send_email(email_address, email_password, recipient_name, recipient_email)
                 pbar.update(1)
-
-        print("所有邮件发送完毕。")
 
     def send_email(self, email_address, email_password, recipient_name, recipient_email):
         smtp_host, smtp_port = get_smtp_settings(email_address)
@@ -37,7 +33,8 @@ class EmailSenderApp:
         msg['To'] = recipient_email
 
         try:
-            with open('web/未录取通知.html', 'r', encoding='utf-8') as f:
+            __path__ = f'web/{input("录取 or 未录取")}通知.html'
+            with open(__path__, 'r', encoding='utf-8') as f:
                 html_content = f.read()
 
             html_content = html_content.replace('{{name}}', recipient_name)
@@ -45,9 +42,9 @@ class EmailSenderApp:
             msg.add_alternative(html_content, subtype='html')
             smtp.login(email_address, email_password)
             smtp.send_message(msg)
-            print(f"成功发送给 {recipient_name} ({recipient_email})")
+            print(f"successfully sent email to {recipient_name} ({recipient_email})")
         except Exception as e:
-            print(f"发送邮件失败: {e}")
+            print(f"failed to send email to {recipient_name} ({recipient_email}): {e}")
         finally:
             smtp.quit()
 
@@ -55,9 +52,6 @@ def main():
     EmailSenderApp()
 
 if __name__ == '__main__':
-    print("----------------")
-    print("南京工业大学校大学生科学技术协会面试结果通知")
-    print("确认发送的内容无误，文件路径正确，请输入 y 开始发送。")
     question = input("是否开始发送邮件？(y/n)")
     if question.lower() == 'y':
         main()
